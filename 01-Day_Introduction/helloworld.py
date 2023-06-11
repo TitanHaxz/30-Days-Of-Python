91f9f66fd55792f78ceddc785a96f0975b8f7ec0
TitanHaxz <titanhaxz@gmail.com>
2023-06-11 10:41:21 +0300

    01-day challenge has been completed

---
+++ 01-Day_Introduction/helloworld.py
@@ -0,0 +1,33 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print(3 + 4)    # addition
+print(3 - 4)    # subtraction
+print(3 * 4)    # multiplication
+print(3 % 4)    # modulus
+print(3 / 4)    # division
+print(3 ** 4)   # exponential
+print(3 // 4)   # floor division operato
+
+# Writing strings
+
+name = "My name"
+family_name = "My family name"
+country = "My country"
+message = "I am enjoying 30 days of python"
+
+print(name)
+print(family_name)
+print(country)
+print(message)
+
+# Checking data types
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('MyName'))
+print(type('MyFamilyName'))
+print(type('Turkey'))
